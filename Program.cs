f71d6dbd1c6be102f900fd963078fc6f1060bcba
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-23 15:50:54 +0500

    create_1.1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿Console.Write("Введите размер массива строк: ");
 int n = int.Parse(Console.ReadLine());
 string[] arr = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"Введите строку {i + 1}: ");
+    arr[i] = Console.ReadLine();
+}
+
+string[] result = new string[n];
+int index = 0;
+foreach (string str in arr)
+{
+    if (str.Length <= 3)
+    {
+        result[index] = str;
+        index++;
+    }
+}
